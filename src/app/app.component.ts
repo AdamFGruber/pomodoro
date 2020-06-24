c718f90abcdf6f84b0ce5465d6db70fe78e9b296
Adam Gruber <adam.gruber@cgi.com>
2020-06-24 14:42:56 +0100

    basic working version of app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,64 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'pomodoro';
+  title: string = 'pomodoro';
+  active: boolean = false
+  worktime: number = 25
+  breaktime: number = 5
+  work: boolean = true
+  timeleft: number = 0
+  interval
+  timeString: string = ""
+
+  validateInput() {
+    if (this.worktime > 0 && this.breaktime > 0) {
+      return true
+    }
+    else {
+      console.log("invalid input")
+      return false
+    }
+  }
+
+  start(worktime, breaktime) {
+    this.worktime = worktime
+    this.breaktime = breaktime
+
+    if (this.validateInput()) {
+      this.active = true
+      this.work ? this.startTimer(this.worktime) : this.startTimer(this.breaktime)
+    }
+  }
+
+  stop() {
+    clearInterval(this.interval);
+    this.timeleft = 0
+    this.work = true
+    this.active = false
+  }
+
+  startTimer(minutes) {
+    this.timeleft = Math.floor(minutes * 60)
+    this.setTimeString(this.timeleft)
+    this.interval = setInterval(() => {
+      if (this.timeleft > 1) {
+        this.timeleft--;
+        this.setTimeString(this.timeleft)
+      } else {
+        clearInterval(this.interval);
+        this.work = !this.work
+        this.work ? this.startTimer(this.worktime) : this.startTimer(this.breaktime)
+      }
+    }, 1000)
+  }
+
+  setTimeString(time) {
+    let mins = Math.floor(time / 60)
+    let secs = Math.floor(time % 60)
+    this.timeString =
+      mins + (mins == 1 ? " minute, " : " minutes, ") +
+      secs + (secs == 1 ? " second" : " seconds")
+  }
 }
