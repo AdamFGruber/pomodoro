38b130a28f47dd4b0cc252551124ff3e57481c35
Adam Gruber <adam.gruber@cgi.com>
2020-07-06 16:50:38 +0100

    added some minor tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -11,4 +11,30 @@ describe('workspace-project App', () => {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual('Welcome to pomodoro!');
   });
+
+  it('should have a title', function () {
+    page.navigateTo();
+    expect(page.getTitle()).toEqual('Pomodoro');
+  });
+
+  it('start button should have text', function () {
+    page.navigateTo();
+    expect(page.getStartButton().getText()).toEqual('START POMODORO!');
+  });
+
+  it('form is visible', function () {
+    page.navigateTo();
+    expect(page.getFormDiv().isDisplayed()).toBe(true);
+  });
+
+  // Fix Async error. Times out
+  // it('timer is visible once started', function () {
+  //   page.navigateTo();
+
+  //   page.getWorkInput().sendKeys(2);
+  //   page.getBreakInput().sendKeys(5);
+  //   page.getStartButton().click();
+
+  //   expect(page.getTimerDiv().isDisplayed()).toBe(true);
+  // });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -8,4 +8,28 @@ export class AppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getTitle() {
+    return browser.getTitle()
+  }
+
+  getStartButton() {
+    return element(by.id('startButton'))
+  }
+
+  getFormDiv() {
+    return element(by.id('formDiv'))
+  }
+
+  getTimerDiv() {
+    return element(by.id('timerDiv'))
+  }
+
+  getWorkInput() {
+    return element(by.id('workInput'))
+  }
+
+  getBreakInput() {
+    return element(by.id('breakInput'))
+  }
 }
